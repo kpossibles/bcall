8e1f36fd56ce5979b350eddfbe08ab41053d7fb4
Kim <kpossibles@users.noreply.github.com>
2018-12-13 16:59:33 -0600

    fixed logic, everything now added

--- facade/index.js
+++ facade/index.js
@@ -41,14 +41,14 @@ module.exports=( () => {
         let res='';
         try{
             if(typeof opts.id!='undefined') {
-                if(opts.desc!='' || opts.name!=''){
-                    if(opts.name!='')
-                        res = mystmts.change.name.run({name : opts.name, id : opts.id}); 
-                    else if(opts.desc!='')
-                        res = mystmts.change.desc.run({desc : opts.desc, id : opts.id}); 
-                }
-                else
-                    res = mystmts.change.all.run(opts);
+              if(opts.desc!='' && opts.name!=''){
+                  console.log("in facade.edit all");
+                  res = mystmts.change.all.run(opts); 
+              } else if(opts.desc == '')
+                res = mystmts.change.name.run({name : opts.name, id : opts.id}); 
+              else if(opts.name == '')
+                res = mystmts.change.desc.run({desc : opts.desc, id : opts.id});
+                
                 if(res.changes != 1) throw new Error();
                 console.log(`FACADE EDIT: Changes made on ${opts.id}!`);
                 return true;
